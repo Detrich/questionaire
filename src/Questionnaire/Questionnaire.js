8378151029c3e26c938dfad3cb3a3d9a8b784cc8
Detrich <detrich2k@gmail.com>
2020-10-30 09:05:48 -0400

    Added a condition if you would not like to play again, also updated readme

--- src/Questionnaire/Questionnaire.js
+++ src/Questionnaire/Questionnaire.js
@@ -2,6 +2,7 @@ import data from "../Apprentice_TandemFor400_Data.json";
 import { useState } from "react";
 import "./Questionnaire.css";
 
+//result array
 let res = [];
 
 //question,incorrect,correct
@@ -11,9 +12,11 @@ function Questionnaire() {
   const [highscore, setHighscore] = useState(0);
   const [wrong, setWrong] = useState(0);
   const [WrongAnswers, setWrongAnswers] = useState(["•", "•", "•"]);
+
   let defaultClassName = "ansButtons";
 
   //randomizes our questions to be mixed up.
+  // uses a set so that we don't have multiple answers generated
   let set = new Set(question.incorrect).add(question.correct);
   let randomizeAnswers = [...set].sort(function (a, b) {
     return Math.random() - 0.5;
@@ -26,16 +29,14 @@ function Questionnaire() {
     let randomKey = objectkeys[Math.floor(Math.random() * objectkeys.length)];
 
     if (!res.includes(answer)) {
-
       //if our res does not have our answer in it, push it in
       res.push(answer);
-      console.log(res)
       // if our res has the answer already in it
       if (res.includes(data[randomKey].correct)) {
         //while loop for when we already had the random key in our list to create a new number to push into the list
+        //I know this probably isn't the fastest implementation but It works well for this example
         while (res.includes(data[randomKey].correct)) {
-          console.log('question already generated,'+ data[randomKey].correct + ' creating another')
-          //redefine randomkey to select another number for us
+          //fetch a new randomKey
           randomKey = objectkeys[Math.floor(Math.random() * objectkeys.length)];
         }
         return data[randomKey];
@@ -44,31 +45,42 @@ function Questionnaire() {
     return data[randomKey];
   }
 
-  function resetGame(){
+  function resetGame() {
     setScore(0);
+    //sets new highscore
     if (score > highscore) {
       setHighscore(score);
     }
+    //resets the wrong answers
     setWrong(0);
     setWrongAnswers(["•", "•", "•"]);
+    //resets our result array
     res = [];
   }
   //when we click on one of the answers we search if our answer is correct, if not, it's a wrong answer
   function submit(answer) {
+    //if our selection is the same as our answer
     if (question.correct === answer) {
+      //changes the color of the button to green by updating the class name.
       document.getElementById(question.correct).className = "CorrectAns";
       setTimeout(function () {
-        if(score>=highscore){
-          setHighscore(highscore + 1)
+        //sets the highscore === score when I get a new highscore(updates highscore on the spot when you reach a new highscore)
+        if (score >= highscore) {
+          //adds one to highscore
+          setHighscore(highscore + 1);
         }
+        //adds one to the score
         setScore(score + 1);
-        if(score === data.length - 3){
-          if (window.confirm("You win!!!, Would you like to restart?")) {
-            resetGame()
-          } else {
 
-          } 
-          setQuestion(getRandomQuestion());
+        //finds if we won the game, I took 3 off because we have three chances to fail.
+        if (score === data.length - 3) {
+          if (window.confirm("You win!!!, Would you like to restart?")) {
+            resetGame();
+          } else {
+            window.location.href = '/'
+          }
+          //resets the question, passing in the correct answer so that we can see if we have had that answer before
+          setQuestion(getRandomQuestion(question.correct));
           setScore(0);
           if (score > highscore) {
             setHighscore(score);
@@ -76,12 +88,9 @@ function Questionnaire() {
           setWrong(0);
           setWrongAnswers(["•", "•", "•"]);
           res = [];
-        }else{
-        setQuestion(getRandomQuestion(question.correct))
+        } else {
+          setQuestion(getRandomQuestion(question.correct));
         }
-
-
-
       }, 800);
     } else {
       document.getElementById(answer).className = "WrongAns";
@@ -90,16 +99,19 @@ function Questionnaire() {
         setQuestion(getRandomQuestion(question.correct));
         setWrong(wrong + 1);
         setWrongAnswers((WrongAnswers) => ["X", ...WrongAnswers.slice(0, -1)]);
+
         if (wrong === 2) {
           if (window.confirm("Defeat... Would you like to restart?")) {
-            resetGame()
+            resetGame();
           } else {
-            console.log(window.location.href)}
-          } 
-      }}, 800);
+            window.location.href = '/'
+          }
+        }
+      },
+        800);
     }
-
   }
+
   return (
     <div className="App">
       <header className="Background">
@@ -119,7 +131,9 @@ function Questionnaire() {
             ))}
           </div>
           <div className="score">
-            <div>{score} out of {data.length - 2}</div>
+            <div>
+              {score} out of {data.length - 2}
+            </div>
             <div> High Score: {highscore}</div>
           </div>
           <div className="WrongAnswers">
